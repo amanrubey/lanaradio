ee7bde980f925904d6a1c8d3440708fb68f73544
amanrubey <aman.rubey@gmail.com>
2023-12-28 20:56:54 +0530

    audio perfect

--- src/app/page.js
+++ src/app/page.js
@@ -2,48 +2,32 @@
 import React from "react";
 import { Card, CardHeader, CardBody, Image, divider } from "@nextui-org/react";
 import { NextUIProvider } from '@nextui-org/react'
-import {Howl, Howler} from 'howler';
-import { useState,useEffect } from 'react';
+import { Howl, Howler } from 'howler';
+import { useState, useEffect } from 'react';
 import { useRef } from 'react';
 import { useSound } from 'use-sound';
 // import boopSfx from '/song.mp3';
 export default function Page() {
-  // let audio = new Audio("/song.mp3")
-  const [value,setValue] = useState(0);
-  const [value2,setValue2] = useState(false);
-  useEffect(()=>{
-    var sound = new Howl({
-      src: ['/song2.mp3'],
-      html5: true
-    });
-    if(value==1 && value2==false)
+
+  var myAudio = new Howl({
+    src: ['/song2.mp3'],
+    html5: true
+  });
+  function togglePlay() {
+    if(myAudio.playing())
     {
-      // Howler.stop();
-      sound.play();
-      setValue2(true);
+      myAudio.pause();
     }
-    else if(value2 == true && value!=0)
-    {
-        // Howler.stop();
-        sound.play();
-        setValue2(false)
+    else{
+      myAudio.play();
     }
-    else {
-      Howler.stop();
-      setValue2(!value2);
-    }
-  },[value,value2])
-  
-  
+
+  };
 
   return (
     <NextUIProvider>
-
       <div className="bg-gradient-to-b from-rose-400 to-rose-700 min-h-screen min-w-screen flex justify-center items-center">
-        {/* <div onClick={playAudio} className="h-24 w-24 bg-white cursor-pointer"> */}
-
-        {/* </div> */}
-        <Card isPressable onMouseEnter={()=>setValue(value+1)} onPress={()=>setValue(!value2)}  className="py-4 bg-opacity-20 text-transparent text-white focus:bg-opacity-0 hover:duration-300  hover:scale-105 focus:scale-125 focus:duration-2000 focus:-translate-x-unit-8xl cursor-pointer">
+        <Card isPressable onPress={togglePlay} className="py-4 bg-opacity-20 text-transparent text-white focus:bg-opacity-0 hover:duration-300  hover:scale-105 focus:scale-125 focus:duration-2000 focus:-translate-x-unit-8xl cursor-pointer">
 
           <CardHeader className="pb-0  pt-2 px-4 flex-col items-start">
             <p className="text-tiny uppercase font-bold">Daily Mix</p>
@@ -60,6 +44,7 @@ export default function Page() {
           </CardBody>
         </Card>
       </div>
+
     </NextUIProvider>
   );
 }
